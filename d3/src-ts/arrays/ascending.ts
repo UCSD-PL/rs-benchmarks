c546761fc2446519763dd005cf92450374a8dccc
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-07-20 21:59:32 +0300

    ii

--- d3/src-ts/arrays/ascending.ts
+++ d3/src-ts/arrays/ascending.ts
@@ -1,6 +1,8 @@
 /// <reference path="../../d3.d.ts" />
-d3.ascending = d3_ascending;
 
+// d3.ascending = d3_ascending;
+
+/*@ d3_ascending :: forall T . (T,T) => number */
 function d3_ascending<T>(a:T, b:T): number {
     return a < b ? -1 : a > b ? 1 : a >= b ? 0 : NaN;
 }
