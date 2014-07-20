0645c0fced785a2ae804f8813a2c1089e032bc5e
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-07-20 21:08:49 +0300

    Adding TS types to d2/arrays/bisect.ts
      - TODO: Need to add support in RefScript to:
    
        f :: ` arrow type `
    
        f.length = { number | v = `the number of arguments of f` }

--- d3/src-ts/arrays/bisect.ts
+++ d3/src-ts/arrays/bisect.ts
@@ -1,44 +1,50 @@
 /// <reference path="../../d3.d.ts" />
 /// <reference path="ascending.ts" />
-function d3_bisector(compare: (data: any, index: number) => any): any {
+
+interface Id3_bisector<T> {
+	left : (a: T[], x: T, lo?: number, hi?: number) => number;
+	right: (a: T[], x: T, lo?: number, hi?: number) => number;
+}
+
+function d3_bisector<T>(compare: (data: T, index?: T) => number): Id3_bisector<T> {
     return {
-    left<T>: function (a: T[], x: T, lo?: number, hi?: number): number {
-            if (arguments.length < 3)
-                lo = 0;
-            if (arguments.length < 4)
-                hi = a.length;
-            while (lo < hi) {
-                var mid = lo + hi >>> 1;
-                if (compare(a[mid], x) < 0)
-                    lo = mid + 1;
-                else
-                    hi = mid;
-            }
-            return lo;
+		left: function(a: T[], x: T, lo?: number, hi?: number): number {
+		if (arguments.length < 3)
+			lo = 0;
+		if (arguments.length < 4)
+			hi = a.length;
+		while (lo < hi) {
+			var mid = lo + hi >>> 1;
+			if (compare(a[mid], x) < 0)
+				lo = mid + 1;
+			else
+				hi = mid;
+		}
+		return lo;
         },
-        right<T>: function (a:T[], x:T, lo?: number, hi?: number): number {
-            if (arguments.length < 3)
-                lo = 0;
-            if (arguments.length < 4)
-                hi = a.length;
-            while (lo < hi) {
-                var mid = lo + hi >>> 1;
-                if (compare(a[mid], x) > 0)
-                    hi = mid;
-                else
-                    lo = mid + 1;
-            }
-            return lo;
-        }
-    };
+	right: function(a: T[], x: T, lo?: number, hi?: number): number {
+		if (arguments.length < 3)
+			lo = 0;
+		if (arguments.length < 4)
+			hi = a.length;
+		while (lo < hi) {
+			var mid = lo + hi >>> 1;
+			if (compare(a[mid], x) > 0)
+				hi = mid;
+			else
+				lo = mid + 1;
+		}
+		return lo;
+	}
+};
 }
 
 var d3_bisect = d3_bisector(d3_ascending);
 d3.bisectLeft = d3_bisect.left;
 d3.bisect = d3.bisectRight = d3_bisect.right;
 
-d3.bisector = function (f: (data: any, index: number) => any): any {
-    return d3_bisector(f.length === 1 ? function (d, x) {
-        return d3_ascending(f(d), x);
-    } : f);
+d3.bisector = function (f: (data: any, index?: number) => any): any {
+	return d3_bisector(f.length === 1
+		? function (d, x) { return d3_ascending(f(d), x); }
+		: f);
 };
