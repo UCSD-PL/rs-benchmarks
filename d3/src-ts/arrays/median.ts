5e7a0a78784ddd97f9f347f342c2fd9876a89579
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-07-23 18:44:35 +0300

    Inferring type for d3/src/ts/arrays/test0.ts

--- d3/src-ts/arrays/median.ts
+++ d3/src-ts/arrays/median.ts
@@ -9,7 +9,11 @@
 // median(arr: number[]): number;
 // median<T>(arr: T[], map: (v: T) => number): number;
 
-d3.median = function<T>(array: T[], f?: (v: T) => T): number {
+d3.median = d3_median;
+
+function d3_median<T>(array: number[]): number;
+function d3_median<T>(array: T[], f?: (v: T) => T): number;
+function d3_median<T>(array: any[], f?: any): number {
   if (arguments.length > 1) array = array.map(f);
   array = array.filter(d3_number);
   return array.length ? d3.quantile(array.sort(d3_ascending), .5) : undefined;
--- d3/src-ts/arrays/test0.ts
+++ d3/src-ts/arrays/test0.ts
@@ -2,8 +2,8 @@
 
 // RJ: I tried adding this but it still fails. At any rate, we should be able to
 // check this WITHOUT requiring the below annotation.
-/*@ d3_descending :: forall T. (a:T, b:T) => number */
+/*  d3_descending :: forall T. (a:T, b:T) => number */
 
-var d3_descending = function<T>(a:T, b:T): number {
+var d3_descending = function <T>(a: T, b: T): number {
   return b < a ? -1 : b > a ? 1 : b >= a ? 0 : NaN;
 };
