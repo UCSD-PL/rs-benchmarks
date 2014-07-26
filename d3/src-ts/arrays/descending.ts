d9f30d4f98e52a9d2d06576c63522e07f9a7d6d6
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-07-26 16:45:57 -0700

    adding XXX-0.ts files for versions kicked through rsc.

--- d3/src-ts/arrays/descending.ts
+++ d3/src-ts/arrays/descending.ts
@@ -1,4 +1,13 @@
 /// <reference path="../../d3.d.ts" />
+
+function foo(x:number):number {
+    return x + 1;
+}
+
+function bar(z:number):number {
+    return foo(z);
+}
+
 d3.descending = function<T>(a:T, b:T): number {
   return b < a ? -1 : b > a ? 1 : b >= a ? 0 : NaN;
 };
