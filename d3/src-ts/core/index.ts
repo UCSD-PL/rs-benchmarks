63572e9abc2d17988dfcd05240c5fb9002b765f3
Benjamin Cosman <bcosman@caltech.edu>
2014-11-19 16:25:06 -0800

    Cleanup d3:core/index.ts

--- d3/src-ts/core/index.ts
+++ d3/src-ts/core/index.ts
@@ -1,14 +1,4 @@
 /// <reference path="../../d3.d.ts" />
-
-// This
-
 /// <reference path="functor.ts" />
-
-// is 
 /// <reference path="ns.ts" />
 /// <reference path="rebind.ts" />
-
-
-
-// JUNK
-
