9d4129136cefb6e15c3ae66df81051e4f0cb8408
Panagiotis Vekris <pvekris@cs.ucsd.edu>
2014-11-26 22:23:37 -0800

    Fixed raytrace

--- octane/done/raytrace-typed-octane.ts
+++ octane/done/raytrace-typed-octane.ts
@@ -405,7 +405,7 @@ module VERSION {
                 this.material = material;
             }
 
-            /*@ intersect : (this: Self<ReadOnly>, ray:Ray<ReadOnly>) : {IntersectionInfo<Immutable> | true} */
+            /*@ intersect : (this: Self<Immutable>, ray:Ray<ReadOnly>) : {IntersectionInfo<Immutable> | true} */
             public intersect(ray:Ray) : IntersectionInfo {
                 throw "Abstract method";
             }
@@ -420,7 +420,7 @@ module VERSION {
                 this.radius = radius;
             }
 
-            /*@ intersect : (this: Self<ReadOnly>, ray:Ray<ReadOnly>) : {IntersectionInfo<Immutable> | true} */
+            /*@ intersect : (this: Self<Immutable>, ray:Ray<ReadOnly>) : {IntersectionInfo<Immutable> | true} */
             public intersect(ray:Ray) : IntersectionInfo {
                 var info = new IntersectionInfo(false, 0, null, null, null, null, null);
                 info.shape = <Shape>this;
@@ -470,7 +470,7 @@ module VERSION {
                 this.d = d;
             }
 
-            /*@ intersect : (this: Self<ReadOnly>, ray:Ray<ReadOnly>) : {IntersectionInfo<Immutable> | true} */
+            /*@ intersect : (this: Self<Immutable>, ray:Ray<ReadOnly>) : {IntersectionInfo<Immutable> | true} */
             public intersect(ray:Ray) : IntersectionInfo {
                 var info = new IntersectionInfo(false, 0, null, null, null, null, null);
 
